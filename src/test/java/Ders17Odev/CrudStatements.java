09bd77a26375afef02b9d0515bd52a108a6735fd
ceryigit <ceryigit@lely.com>
2024-08-06 02:02:09 +0300

    Ders17 ÖdevEdited

--- src/test/java/Ders17Odev/CrudStatements.java
+++ src/test/java/Ders17Odev/CrudStatements.java
@@ -10,39 +10,42 @@ import java.util.OptionalLong;
 public class CrudStatements {
     public static void main(String[] args) {
 
-        String url = "jdbc:mysql://localhost:3306/new_schema";
-        String kullaniciAdi = "root";
-        String sifre = "my-secret-pw";
+        String url = "jdbc:mysql://sql7.freemysqlhosting.net/sql7724182";
+        String kullaniciAdi = "sql7724182";
+        String sifre = "msEppxHyKX";
 
         Jdbi jdbi = Jdbi.create(url, kullaniciAdi, sifre);
-        try  {
-            Handle handle = jdbi.open();
+        try (Handle handle = jdbi.open()) {
             System.out.println("Database bağlantısı başarılı.");
-
+            //Tablo Oluşturma
             handle.execute("CREATE TABLE IF NOT EXISTS Departments (dptName VARCHAR(100), nrOfMembers int)");
+            //Tabloya Yeni Kayıt Ekleme
             int eklenenSatir =  handle.createUpdate("INSERT INTO Departments (dptName, nrOfMembers) VALUES (:dptName, :nrOfMembers)")
                     .bind("dptName", "IT")
                     .bind("nrOfMembers", 15)
                     .execute();
             System.out.println("Eklenen kayıt sayısı: " + eklenenSatir);
-
-            String department = handle.createQuery("SELECT * FROM new_schema.Departments")
-                    .mapToMap(String.class)
-                    .one().toString();
-            System.out.println("Departments tablosuna eklenen kayıt: " + department);
-
-            Optional<Long> updateResult = handle.createUpdate("UPDATE Departments SET nrOfMembers = 12 WHERE dptName = :dptname")
-                    .bind("dptname", "IT")
-                    .executeAndReturnGeneratedKeys()
-                    .mapTo(Long.class)
+            //Tablodaki Kayıtları Okuma
+            Optional<Map<String, Object>> department = handle.createQuery("SELECT * FROM Departments")
+                    .mapToMap()
                     .findFirst();
-            System.out.println("Kayıt update edildi.");
 
-
-            int satirSil = handle.createUpdate("DELETE FROM Employee WHERE id = :id")
-                    .bind("id", 3)
+            if (department.isPresent()) {
+                System.out.println("Departments tablosuna eklenen kayıt: " + department.get());
+            } else {
+                System.out.println("Departments tablosunda kayıt bulunamadı.");
+            }
+            //Kayıt Güncelleme
+            int updatedRows = handle.createUpdate("UPDATE Departments SET nrOfMembers = :nrOfMembers WHERE dptName = :dptName")
+                    .bind("dptName", "IT")
+                    .bind("nrOfMembers", 12)
                     .execute();
-            System.out.println("Silinen kayıt: " + satirSil);
+            System.out.println("Güncellenen kayıt sayısı: " + updatedRows);
+            //Kayıt Silme
+            int satirSil = handle.createUpdate("DELETE FROM Departments WHERE dptName = :dptName")
+                    .bind("dptName", "IT")
+                    .execute();
+            System.out.println("Silinen kayıt sayısı: " + satirSil);
         } catch (StatementException e) {
             e.printStackTrace();
         }
---
+++ src/test/java/Ders18Odev/MaasHesaplaTest.java
@@ -0,0 +1,7 @@
+package Ders18Odev;
+
+public class MaasHesaplaTest {
+
+
+
+}
